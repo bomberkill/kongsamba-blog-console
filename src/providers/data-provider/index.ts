cc047b4f6b5c29428b8dae9080e78da4196d6148
Bomberkill <ronaldkamgaing4@gmail.com>
2025-01-02 00:34:46 +0100

    fix: api connection

--- src/providers/data-provider/index.ts
+++ src/providers/data-provider/index.ts
@@ -5,7 +5,7 @@ import { DELETE_ARTICLE, DELETE_EVENT, DELETE_PLAYLIST } from "@queries";
 import { BaseRecord, CreateParams, CreateResponse, DataProvider, DeleteOneParams, DeleteOneResponse, GetListParams, GetListResponse, GetOneParams, GetOneResponse, UpdateParams, UpdateResponse } from "@refinedev/core";
 import graphqlDataProvider, { GraphQLClient } from "@refinedev/graphql";
 
-const API_URL = "http://18.212.31.31:8080/graphql";
+const API_URL = "http://3.235.63.122:8080/graphql";
 
 const transformLink = new ApolloLink((operation, forward) => {
     return forward(operation).map((response) => {
